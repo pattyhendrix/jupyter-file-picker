5d27dabd9022ea9054bf27d435a289a1eb9bdc30
zc <zachcaceres@gmail.com>
2018-10-29 13:57:18 -0700

    feat: styling and reorganize around single button click workflow where images are flagged on click and deleted on confirm

--- index.py
+++ index.py
@@ -1,7 +1,11 @@
 # TODO: re-render UI after we delete some photos
 # TODO: styling of grid to make it nicer to navigate
 # TODO: Write the readme
-# TODO: add progress bar
+#
+# TODO: Come back to this so function can accept data bunch or top losses
+# if file_paths == top_losses:
+#     file_paths = data.valid_ds.x[interp.top_losses(10)[1]]
+
 
 from ipywidgets import widgets, Layout
 from IPython.display import clear_output, HTML
@@ -13,19 +17,14 @@ def render_file_list(file_paths, button_label="Delete Selected", checkbox_label=
         On button click, deletes all selected images.
     """
 
-    # TODO: Come back to this so function can accept data bunch or top losses
-    # if file_paths == top_losses:
-    #     file_paths = data.valid_ds.x[interp.top_losses(10)[1]]
-
-
-    image_blocks = []
+    render_list = []
+    batch = []
 
     def main(file_paths):
         for fp in file_paths:
             img = make_img(fp)
-            keep_button = make_button('Keep', fp, on_keep)
-            delete_button = make_button('Delete', fp, on_delete)
-            image_blocks.append((img, keep_button, delete_button, fp))
+            delete_button = make_button('Delete', file_path=fp, handler=on_delete)
+            render_list.append((img, delete_button, fp))
         render()
 
     def make_img(file_path):
@@ -35,39 +34,52 @@ def render_file_list(file_paths, button_label="Delete Selected", checkbox_label=
         opened_file.close()
         return img
 
-    def make_checkbox(checkbox_label, file_path):
-        cb = widgets.Checkbox(value=False, description=checkbox_label)
-        cb.file_path = file_path
-        return cb
-
-    def on_keep(btn):
-        remove_from_list(btn.file_path)
+    def on_confirm(btn):
+        to_remove = []
+        for img, delete_btn, fp in render_list:
+            fp = delete_btn.file_path
+            if (delete_btn.flagged_for_delete == True):
+                delete_image(fp)
+            to_remove.append((img, delete_btn, fp))
+        for img, delete_btn, fp in to_remove:
+            render_list.remove((img, delete_btn, fp))
         render()
 
-    def remove_from_list(file_path):
-        to_remove = []
-        for img, keep_btn, delete_btn, fp in image_blocks:
-            if file_path == fp:
-                to_remove.append((img, keep_btn, delete_btn, fp))
-        for img, keep_btn, delete_btn, fp in to_remove:
-            image_blocks.remove((img, keep_btn, delete_btn, fp))
+    def delete_image(file_path):
+        print('deleting', file_path)
+        # TODO: REENABLE ME AFTER TESTING! os.remove(file_path)
 
     def on_delete(btn):
-        os.remove(btn.file_path)
-        remove_from_list(btn.file_path)
-        render()
+        """
+            Flags this image as delete or keep.
+        """
+        if (btn.flagged_for_delete is True):
+            btn.flagged_for_delete = False
+            btn.description = "Keep"
+            btn.button_style = "success"
+        else:
+            btn.flagged_for_delete = True
+            btn.description = "Delete"
+            btn.button_style = "danger"
 
-    def make_button(label, file_path, handler):
+    def make_button(label, file_path=None, handler=None, style=None):
         btn = widgets.Button(description=label)
-        btn.on_click(handler)
+        if (handler is not None):
+            btn.on_click(handler)
+        if (style is not None):
+            btn.button_style = style
         btn.file_path = file_path
+        btn.flagged_for_delete = False
         return btn
 
     def render():
         clear_output()
         children = []
-        for img, keep_btn, delete_btn, fp in image_blocks:
-            children.append(widgets.VBox([img, keep_btn, delete_btn], layout=Layout(width='250px')))
+        if (len(render_list) == 0):
+            return display('No images to show :)')
+        for img, delete_btn, fp in render_list:
+            children.append(widgets.VBox([img, delete_btn], layout=Layout(width='250px')))
         display(widgets.HBox(children[:5]))
+        display(make_button('Confirm', handler=on_confirm, style="primary"))
 
     main(file_paths)
